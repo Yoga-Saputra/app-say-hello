b52c423e0d701c5c0099a927e5992118fc9d00a7
Yoga-Saputra <yogasaputran@gmail.com>
2022-07-25 23:05:28 +0700

    update package

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Yoga-Saputra/app-say-hello
 
 go 1.18
 
-require github.com/Yoga-Saputra/go-say-hello v1.6.0
+require github.com/Yoga-Saputra/go-say-hello/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	go_say_hello "github.com/Yoga-Saputra/go-say-hello"
+	go_say_hello "github.com/Yoga-Saputra/go-say-hello/v2"
 )
 
 // add dependency : go get github.com/Yoga-Saputra/go-say-hello
 // update dependency : only update tag version in go.mod and run go get in terminal
 
 func main() {
-	fmt.Println(go_say_hello.SayHello())
+	fmt.Println(go_say_hello.SayHello("Yoga Saputra"))
 }
